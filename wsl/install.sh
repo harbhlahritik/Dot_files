e517084664c308f53771d5a78be7e4557b4c88cf
Hritik Harbhla <harbhlahritik@gmail.com>
2020-07-13 16:55:27 +0530

    made install.sh script and made some changes to .bashrc

--- wsl/install.sh
+++ wsl/install.sh
@@ -22,17 +22,58 @@ install tree
 install vim
 install xclip
 
-#get java8
-install openjdk-8-jdk-headless
-
-# install papirus icons
-sudo add-apt-repository ppa:papirus/papirus
-install papirus-icon-theme
+#get java11
+install openjdk-11-jdk-headless
 
 #install node js
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+install nodejs
 
-# Fun stuff
-install figlet
-install lolcat
+# install rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
+7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
+rvm get stable --autolibs=enable
+usermod -a -G rvm root
+rvm install ruby
+install gcc g++ make
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+install yarn nodejs
+gem update --system
+gem install rails
+
+
+# install zsh
+install zsh
+install powerline fonts-powerline
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo chsh -s $(which zsh)
+
+# install spotify
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && install spotify-client
+
+#install chrome
+install software-properties-common apt-transport-https wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+#install vs code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+install code
+
+# git config user.email and user.name
+git config --global user.email "harbhlahritik@gmail.com"
+git config --global user.name "Hritik Harbhla"
+
+# generate ssh keys and copy to clipboard
+ssh-keygen -t rsa -b 4096 -C "harbhlahritik@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+xclip -sel clip < ~/.ssh/id_rsa.pub
